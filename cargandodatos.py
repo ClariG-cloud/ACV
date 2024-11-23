77969d59b2b4ddf55013b45c1510edaf7b5e1e13
ClariG-cloud <you@example.com>
2024-11-23 13:11:05 -0300

    Actualización de cargandodatos.py

--- cargandodatos.py
+++ cargandodatos.py
@@ -14,59 +14,68 @@ st.title("Aplicación para predicción de ACV")
 
 # Crear las opciones para las entradas del usuario
 # Género
-opciones_genero = ["Female", "Male"]
+opciones_genero = ["Femenino", "Masculino"]
 genero = st.selectbox("Selecciona el género con el que te sientes identificado", opciones_genero)
 
-# Hipertensión (0 = No, 1 = Sí)
-hipertension = st.selectbox("Pon 0 si no tienes hipertensión o 1 si tienes", ["0", "1"])
+hipertension = st.radio(
+    "¿Usted padece hipertensión?",  # Etiqueta para el input
+    ["Sí, tengo hipertensión", "No, no tengo hipertensión"]  # Opciones
+)
 
-# Cardiopatía (0 = No, 1 = Sí)
-cardiopatia = st.selectbox("Selecciona 1 si padeces alguna cardiopatía o 0 si no", ["0", "1"])
-
+cardiopatia = st.radio(
+    "¿Usted tiene alguna cardiopatía?",  # Etiqueta para el input
+    ["Sí, padezco de cardiopatía", "No, no padezco de cardiopatías"]  # Opciones
+)
 # Hábito de fumar
-opciones_fumar = ["Never smoked", "Smokes", "No smokes", "Formerly smoked"]
+opciones_fumar = ["Nunca fumé", "Fumo", "No fumo", "Antiguamente fumaba"]
 humo = st.selectbox("Selecciona el hábito de consumo de tabaco que posees", opciones_fumar)
 
 # Edad (valor numérico)
-edad = st.slider("Selecciona tu edad:", min_value=1, max_value=100, value=25)
+edad = st.slider("Selecciona tu edad:", min_value=1, max_value=82, value=25)
 
 # IMC (Índice de masa corporal)
-imc = st.slider("Selecciona tu IMC:", min_value=10, max_value=100, value=20)
+imc = st.slider("Selecciona tu IMC:", min_value=10, max_value=98, value=20)
 
 # Convertir las entradas a valores numéricos
 # Convertir género en 1 o 0
-genero_valor = 1 if genero == "Female" else 0
+genero_valor = 1 if genero == "Femenino" else 0
 
-# Convertir hipertensión y cardiopatía a 0 o 1
-hipertension_valor = int(hipertension)
-cardiopatia_valor = int(cardiopatia)
 
+if hipertension == "Sí, tengo hipertensión":
+    hipertension_val = 1
+else:
+    hipertension_val = 0
+if  cardiopatia == "Sí, padezco de cardiopatía":
+    cardiopatia_valor = 1
+else:
+    cardiopatia_valor = 0
 # Convertir hábito de fumar en valores numéricos
-if humo == "Never smoked":
+if humo == "Nunca fumé":
     never_smoked = 1
     no_smokes = 0
     formerly_smoked = 0
     smokes = 0
-elif humo == "No smokes":
+elif humo == "No fumo":
     never_smoked = 0
     no_smokes = 1
     formerly_smoked = 0
     smokes = 0
-elif humo == "Formerly smoked":
+elif humo == "Antiguamente fumaba":
     never_smoked = 0
     no_smokes = 0
     formerly_smoked = 1
     smokes = 0
-else:  # "Smokes"
+else:  
     never_smoked = 0
     no_smokes = 0
     formerly_smoked = 0
     smokes = 1
 
+
 # Asegurarse de que las entradas sean números
 caracteristicas = [
     float(edad),  # Convertir edad a flotante
-    int(hipertension_valor),  # Hipertensión como entero
+    int(hipertension_val),  # Hipertensión como entero
     int(cardiopatia_valor),  # Cardiopatía como entero
     float(imc),  # Convertir IMC a flotante
     int(genero_valor),  # Género como entero
@@ -76,13 +85,14 @@ caracteristicas = [
     int(smokes)  # Fuma como entero
 ]
 
-# Realizar la predicción
-prediccion = modelo_entrenado.predict([caracteristicas])[0]
+if st.button("Determinar riesgo de ACV"):
+    # Llamar al modelo para predecir
+    prediccion = modelo_entrenado.predict([caracteristicas])[0]
+    # Mostrar el resultado de la predicción
+    if prediccion == 0:
+        st.success("No tienes riesgo de sufrir ACV.")
+    else:
+        st.warning("Tienes riesgo de sufrir ACV.")
 
-# Mostrar el resultado de la predicción
-if prediccion == 0:
-    st.write("No tienes riesgo de sufrir ACV.")
-else:
-    st.write("Tienes riesgo de sufrir ACV.")
 
 
